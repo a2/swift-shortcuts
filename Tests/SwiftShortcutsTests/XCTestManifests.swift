efc754ca706ef1dfe67cccd3fc7355d0c1911f27
Alexsander Akers <me@a2.io>
2020-06-27 15:30:18 -0400

    Update test manifests

--- Tests/SwiftShortcutsTests/XCTestManifests.swift
+++ Tests/SwiftShortcutsTests/XCTestManifests.swift
@@ -3,7 +3,8 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(SwiftShortcutsTests.allTests),
+        testCase(BatteryLevelTests.allTests),
+        testCase(ClapAlongTests.allTests),
     ]
 }
 #endif
