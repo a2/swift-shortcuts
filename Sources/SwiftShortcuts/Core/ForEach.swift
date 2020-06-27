59e2738fac85c273272663bd383a052b9ca3c3a5
Alexsander Akers <me@a2.io>
2020-06-27 15:28:10 -0400

    Rename ForEach initializer collection argument

--- Sources/SwiftShortcuts/Core/ForEach.swift
+++ Sources/SwiftShortcuts/Core/ForEach.swift
@@ -4,7 +4,7 @@ public struct ForEach<Data, Content>: Action where Data: Collection, Content: Ac
     public typealias Body = Never
     public var body: Never { fatalError() }
 
-    public init(_ collection: Data, @ActionBuilder builder: (Data.Element) -> Content) {
-        self.children = collection.map(builder)
+    public init(_ data: Data, @ActionBuilder builder: (Data.Element) -> Content) {
+        self.children = data.map(builder)
     }
 }
