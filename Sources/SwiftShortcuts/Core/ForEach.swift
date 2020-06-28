2acaef822cc1c2cba7f617bb5d1e5b81a4b69bd1
Alexsander Akers <me@a2.io>
2020-06-27 20:07:05 -0400

    Revert changes to and simplify ForEach

--- Sources/SwiftShortcuts/Core/ForEach.swift
+++ Sources/SwiftShortcuts/Core/ForEach.swift
@@ -1,10 +1,10 @@
-public struct ForEach<Data, Content>: Action where Data: Collection, Content: Action {
-    let children: [Content]
+public struct ForEach: Action {
+    let children: [AnyAction]
 
     public typealias Body = Never
     public var body: Never { fatalError() }
 
-    public init(_ data: Data, @ActionBuilder builder: (Data.Element) -> Content) {
-        self.children = data.map(builder)
+    public init<Data, Content>(_ data: Data, @ActionBuilder builder: (Data.Element) -> Content) where Data: Collection, Content: Action {
+        self.children = data.map { item in AnyAction(builder(item)) }
     }
 }
