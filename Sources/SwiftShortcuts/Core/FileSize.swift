7255c470991371700d4e3a28fe215d3833b5bee1
Alexsander Akers <me@a2.io>
2020-06-30 20:31:59 -0400

    Remove askEachTime case from ByteCountUnit

--- Sources/SwiftShortcuts/Core/FileSize.swift
+++ Sources/SwiftShortcuts/Core/FileSize.swift
@@ -2,41 +2,15 @@ enum UnitType: Int, Encodable {
     case bytes = 4
 }
 
-public enum ByteCountUnit: Encodable {
-    case bytes
-    case KB
-    case MB
-    case GB
-    case TB
-    case PB
-    case EB
-    case ZB
-    case askEachTime
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-
-        switch self {
-        case .bytes:
-            try container.encode(1)
-        case .KB:
-            try container.encode(2)
-        case .MB:
-            try container.encode(4)
-        case .GB:
-            try container.encode(8)
-        case .TB:
-            try container.encode(16)
-        case .PB:
-            try container.encode(32)
-        case .EB:
-            try container.encode(64)
-        case .ZB:
-            try container.encode(128)
-        case .askEachTime:
-            try container.encode(Variable.askEachTime)
-        }
-    }
+public enum ByteCountUnit: Int, Encodable {
+    case bytes = 1
+    case KB = 2
+    case MB = 4
+    case GB = 8
+    case TB = 16
+    case PB = 32
+    case EB = 64
+    case ZB = 128
 }
 
 public struct FileSize: Encodable {
@@ -47,11 +21,21 @@ public struct FileSize: Encodable {
     }
 
     public var number: InterpolatedText?
-    public var unit: ByteCountUnit
+    public var unit: VariableValue<ByteCountUnit>
+
+    public init(number: InterpolatedText?, unit: VariableValue<ByteCountUnit>) {
+        self.number = number
+        self.unit = unit
+    }
 
     public init(number: InterpolatedText?, unit: ByteCountUnit) {
         self.number = number
-        self.unit = unit
+        self.unit = .value(unit)
+    }
+
+    public init(number: InterpolatedText?, unit: Variable) {
+        self.number = number
+        self.unit = .variable(unit)
     }
 
     public func encode(to encoder: Encoder) throws {
