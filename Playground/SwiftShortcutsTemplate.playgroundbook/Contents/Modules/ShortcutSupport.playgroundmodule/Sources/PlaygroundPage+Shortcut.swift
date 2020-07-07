d352ed23c24f142f2e46546e4c864f0b0e4e011b
Liam Nichols <liam.nichols.ln@gmail.com>
2020-07-07 21:53:03 +0100

    Simplify PlaygroundPage.show(_:) and add documentation

--- Playground/SwiftShortcutsTemplate.playgroundbook/Contents/Modules/ShortcutSupport.playgroundmodule/Sources/PlaygroundPage+Shortcut.swift
+++ Playground/SwiftShortcutsTemplate.playgroundbook/Contents/Modules/ShortcutSupport.playgroundmodule/Sources/PlaygroundPage+Shortcut.swift
@@ -3,31 +3,10 @@ import SwiftShortcuts
 import UIKit
 
 public extension PlaygroundPage {
+    /// Shows a preview of a given shortcut with the ability to share into other apps such as Shortcuts.app.
+    ///
+    /// - Parameter shortcut: The constructed shortcut to be previewed.
     func show<S: Shortcut>(_ shortcut: S) {
-        // Construct the view controller
-        let viewController = ShortcutPreviewViewController(
-            shortcut: shortcut
-        )
-
-        // Wrap the preview within a navigation controller
-        let navigationController = UINavigationController(
-            rootViewController: viewController
-        )
-
-        // Create a new trait collection that secifies `.compact` horizontal size class
-        let traitCollection = UITraitCollection(traitsFrom: [
-            .current,
-            UITraitCollection(horizontalSizeClass: .compact)
-        ])
-
-        // Hide the nav bar, force a compact horizontal environment
-        navigationController.isNavigationBarHidden = true
-        navigationController.setOverrideTraitCollection(
-            traitCollection,
-            forChild: viewController
-        )
-
-        // Set the live view
-        liveView = navigationController
+        liveView = ShortcutPreviewViewController(shortcut: shortcut)
     }
 }
